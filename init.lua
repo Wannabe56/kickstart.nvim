de88a6970758af53fbe335bd8fc2cbae526d848e
Wannabe56 <150284089+Wannabe56@users.noreply.github.com>
2026-06-18 16:50:41 +0200

    feat: add keymaps to move lines

--- init.lua
+++ init.lua
@@ -219,6 +219,10 @@ do
   -- or just use <C-\><C-n> to exit terminal mode
   vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+  -- Move line up or down
+  vim.keymap.set('n', '<A-up>', '<cmd>:m -2<CR>')
+  vim.keymap.set('n', '<A-down>', '<cmd>:m +1<CR>')
+
   -- TIP: Disable arrow keys in normal mode
   -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
   -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
