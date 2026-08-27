7395c3ee03a3f46b7b3e624a67518421ff7aa4e3
Wannabe56 <150284089+Wannabe56@users.noreply.github.com>
2026-08-27 11:39:11 +0200

    feat: add lazygit to telescope

--- init.lua
+++ init.lua
@@ -613,6 +613,7 @@ do
   -- Enable Telescope extensions if they are installed
   pcall(require('telescope').load_extension, 'fzf')
   pcall(require('telescope').load_extension, 'ui-select')
+  pcall(require('telescope').load_extension, 'lazygit')
 
   -- See `:help telescope.builtin`
   local builtin = require 'telescope.builtin'
